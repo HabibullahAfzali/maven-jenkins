faf1609443c591d9fc2462d384e22f0f0800d320
HabibAfzali <82744410+HabibullahAfzali@users.noreply.github.com>
2025-02-22 01:58:25 +0100

    scanner method added

--- src/main/java/com/testjenkins/test/TestApplication.java
+++ src/main/java/com/testjenkins/test/TestApplication.java
@@ -3,20 +3,27 @@ package com.testjenkins.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class TestApplication {
 
-	public static void printMessage(String text) {
-
-		System.out.println(text);
+	public static void printMessage( String text) {
+		String nombre;
+		Scanner scanner = new Scanner(System.in);
+		nombre =scanner.next();
+		System.out.println(nombre+" : "+text);
 	}
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(TestApplication.class, args);
-		printMessage("******************************");
-		printMessage("Hola desde Spring boot!");
-		printMessage("******************************");
+		System.out.println("******************************");
+		System.out.println("Su nombre! ");
+
+
+		printMessage("bienvanida al programacion");
+		System.out.println("******************************");
 	}
 
 }
